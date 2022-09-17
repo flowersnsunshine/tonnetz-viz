e64122868d08c7e70a217558b3693999f2776b25
flowersnsunshine <113802051+flowersnsunshine@users.noreply.github.com>
2022-09-17 13:59:00 -0700

    Add a new color scheme

---
+++ color-schemes/warm-breeze.js
@@ -0,0 +1,38 @@
+colorscheme.addScheme("please-give-me-a-name", {
+  "name": "PLEASE GIVE ME A NAME",
+  "background": "#eeaf61",
+  "nodes": {
+    "OFF": {
+      "label": "#ee5d6c",
+      "fill": "#fb9062",
+      "stroke": "#ee5d6c"
+    },
+    "GHOST": {
+      "label": "#ce4993",
+      "fill": "#ee6c5d",
+      "stroke": "#ce4993"
+    },
+    "SUSTAIN": {
+      "label": "#ce4993",
+      "fill": "#ee5d6c",
+      "stroke": "#ce4993"
+    },
+    "ON": {
+      "label": "#6a0d83",
+      "fill": "#ee5d6c",
+      "stroke": "#6a0d83"
+    }
+  },
+  "faces": {
+    "major": {
+      "label-off": "#c0c0c0",
+      "label-on": "#bea866",
+      "fill": "#f9cda4"
+    },
+    "minor": {
+      "label-off": "#c0c0c0",
+      "label-on": "#aa9b68",
+      "fill": "#ffbd99"
+    }
+  }
+});
